75b32a73dd70362547a07d0c9972b33a77050b7e
rockernun <byunguk98@naver.com>
2026-08-04 16:51:08 +0900

    feat: AssetSyncService 인터페이스 수정

--- src/main/java/com/team/independence/asset/service/AssetSyncService.java
+++ src/main/java/com/team/independence/asset/service/AssetSyncService.java
@@ -1,8 +1,10 @@
 package com.team.independence.asset.service;
 
 import com.team.independence.asset.dto.AssetSyncResponse;
+import com.team.independence.asset.dto.SyncJobStatusResponse;
 
 public interface AssetSyncService {
     AssetSyncResponse syncAccounts(Long memberId);
     void syncAll();
+    SyncJobStatusResponse getSyncStatus(String jobId);
 }
