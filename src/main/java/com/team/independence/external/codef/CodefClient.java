15c7c33ed455fa63ba464b16848391fd6bf96a72
rockernun <byunguk98@naver.com>
2026-08-06 16:28:35 +0900

    feat: CodefClient에 카드 목록 조회 메서드 추가

--- src/main/java/com/team/independence/external/codef/CodefClient.java
+++ src/main/java/com/team/independence/external/codef/CodefClient.java
@@ -3,6 +3,7 @@ package com.team.independence.external.codef;
 import com.team.independence.external.codef.dto.CodefAccountRequest;
 import com.team.independence.external.codef.dto.CodefApiResponse;
 import com.team.independence.external.codef.dto.CodefBankAccountResponse;
+import com.team.independence.external.codef.dto.CodefCardResponse;
 import com.team.independence.external.codef.dto.CodefStockAccountResponse;
 import com.team.independence.external.codef.dto.CodefStockFinancialAssetsResponse;
 
@@ -13,4 +14,5 @@ public interface CodefClient {
     CodefBankAccountResponse getBankAccountList(String accessToken, String connectedId, String organization, String birthDate);
     CodefStockAccountResponse getStockAccountList(String accessToken, String connectedId, String organization);
     CodefStockFinancialAssetsResponse getStockFinancialAssets(String accessToken, String connectedId, String organization, String account);
+    CodefCardResponse getCardList(String accessToken, String connectedId, String organization, String cardNo, String cardPassword, String birthDate);
 }
